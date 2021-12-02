5ee5c92ed1aaba84f433d5dceed2cde19cef8e1f
Renaud Gautier <renaud.gautier@360learning.com>
2021-12-02 12:57:36 +0100

    refactor: factorize file parsing logic in an utils function

--- src/2021/01/solver.ts
+++ src/2021/01/solver.ts
@@ -1,20 +1,12 @@
-const fs = require('fs')
 const path = require('path');
+import { parseFileToArray } from "@utils/parseFileToArray";
 import { resolve }  from './01';
 
 const INPUT_PATH = path.join(__dirname, "../../../assets/2021/01/input.txt"); // UGLY
-const NEW_LINE_SEPARATOR = "\n"
 
-const content = fs.readFileSync(INPUT_PATH, 'utf8');
-const depthMeasurements = parseData(content);
+const stringValues = parseFileToArray(INPUT_PATH);
+const depthMeasurements = stringValues.map(value => parseInt(value));
 
 const result = resolve(depthMeasurements);
 console.log(result);
 
-function parseData(fileContent: string){
-    const stringValues = fileContent.split(NEW_LINE_SEPARATOR);
-
-    stringValues.pop();
-
-    return stringValues.map(value => parseInt(value));
-}
---
+++ src/utils/parseFileToArray.ts
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const NEW_LINE_SEPARATOR = "\n"
+
+export function parseFileToArray(filePath: string): Array<string>{
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+
+    const values = fileContent.split(NEW_LINE_SEPARATOR);
+    values.pop(); // last line is empty
+
+    return values;
+}
