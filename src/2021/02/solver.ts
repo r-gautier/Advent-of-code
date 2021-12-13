df007c5f58272fc22550842857f78ba6b388b85f
Renaud Gautier <renaud.gautier@360learning.com>
2021-12-13 14:45:28 +0100

    feat: implement solver for 02 problem

---
+++ src/2021/02/solver.ts
@@ -0,0 +1,15 @@
+const path = require('path');
+import { parseFileToArray } from "@utils/parseFileToArray";
+import { resolve }  from './02';
+
+console.log("02 December 2021");
+console.log("-----")
+
+console.log("Initializing...")
+const INPUT_PATH = path.join(__dirname, "../../../assets/2021/02/input.txt"); // UGLY
+
+const commands = parseFileToArray(INPUT_PATH);
+console.log("Initialization done.")
+
+const firstResult = resolve(commands);
+console.log("First puzzle solution: ", firstResult);
